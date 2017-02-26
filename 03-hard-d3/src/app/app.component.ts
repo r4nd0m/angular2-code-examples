e910bedfd49ee9c85c80edd7d7f53f2fa549aaf9
Vladimir Kalenyuk <vladimir.kalenyuk@gmail.com>
2017-02-26 12:44:48 +0100

    simple d3 bar chart example

--- 03-hard-d3/src/app/app.component.ts
+++ 03-hard-d3/src/app/app.component.ts
@@ -6,4 +6,29 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {}
+export class AppComponent {
+	data: Array<any> = [];
+
+	changeData = () => {
+		this.data = [{
+			"label": "Total Users",
+			"value": Math.random() * 100
+		},
+		{
+			"label": "Requests",
+			"value": Math.random() * 100
+		},
+		{
+			"label": "Timeouts",
+			"value": Math.random() * 100
+		},
+		{
+			"label": "Latches",
+			"value": Math.random() * 100
+		}];
+	}
+
+	constructor(){
+		this.changeData();
+	}
+}
--- 03-hard-d3/src/app/chart/chart.component.ts
+++ 03-hard-d3/src/app/chart/chart.component.ts
@@ -1,6 +1,5 @@
 import {
 	Component,
-	OnInit,
 	OnChanges,
 	AfterViewInit,
 	Input,
@@ -12,32 +11,48 @@ import * as d3 from 'd3';
 
 @Component({
 	selector: 'app-chart',
-	templateUrl: './chart.component.html',
-	styleUrls: ['./chart.component.css']
+	templateUrl: './chart.component.html'
 })
-export class ChartComponent implements OnChanges, OnInit, AfterViewInit {
+export class ChartComponent implements OnChanges, AfterViewInit {
 	@ViewChild('container') element: ElementRef;
+
+	@Input() data: Array<any> = [];
 
 	private htmlElement: HTMLElement;
 	private host;
+	private chart;
+	private initialized: boolean = false;
 
 	constructor() { }
 
-	ngOnInit() {
-	}
-
-	ngOnChanges() {
-		this.render();
+	ngOnChanges(changes) {
+		if (this.initialized) { // make sure change happens after the chart has been initialized
+			this.render();
+		}
 	}
 
 	ngAfterViewInit() {
 		this.htmlElement = this.element.nativeElement;
 		this.host = d3.select(this.htmlElement);
+		this.initialized = true;
 		this.render();
 	}
 
 	private render(): void {
-		d3.select("body").style("background-color", "black");
+		this.chart = this.host
+			.selectAll("div")
+			.data(this.data)
+			.classed("bar", true)
+			.style("width", d => d.value + '%')
+		
+		this.chart
+			.enter()
+				.append("div")
+				.classed("bar", true)
+				.style("width", d => d.value + '%')
+
+		this.chart.exit()
+			.remove();
 	}
 
 }
